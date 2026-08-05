c907440c8000939d6719e7ae3d26add5f051a368
Fahim <fahimyadgir07@gmail.com>
2026-08-06 00:52:23 +0530

    Update some column name and change data type like to before review_data data type is text after chaging to date function

--- Analysis in MySQL Workbench.sql
+++ Analysis in MySQL Workbench.sql
@@ -3,4 +3,19 @@ use E_commerce;
 
 select * from flipkart_orders_for_sql;
 
-drop table flipkart_orders_for_sql;
+SET SQL_SAFE_UPDATES = 0;
+
+SET SQL_SAFE_UPDATES = 1;
+
+alter table flipkart_orders_for_sql
+rename column `ï»¿order_id_pk`  to order_id;
+
+update flipkart_orders_for_sql
+set review_date = str_to_date(review_date,'%d-%m-%Y');
+
+alter table flipkart_orders_for_sql
+modify column rating int;
+
+alter table flipkart_orders_for_sql
+modify column price bigint;
+
